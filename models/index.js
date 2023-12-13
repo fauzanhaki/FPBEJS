76a6eaffaeb1f7b79e14f7336498d6a77efffa00
kevinarsankamto05 <201110097@student.mercubuana-yogya.ac.id>
2023-12-13 19:02:14 +0700

    add

--- models/index.js
+++
@@ -1,9 +0,0 @@
-const { PrismaClient } = require('@prisma/client')
-
-const prisma = new PrismaClient();
-
-module.exports = {
-    users: prisma.user,
-    category: prisma.Category,
-    course: prisma.course
-}
